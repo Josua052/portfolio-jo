a9c27d80d3c19c7225df73ba4ee362cc779610da
Josua052 <josuaronaldo96@gmail.com>
2026-03-24 16:35:44 +0700

    feature/fixing-ui3: update next

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,22 @@ import ContactSection from "@/components/sections/Home/ContactSection";
 export const metadata = {
   title: "Portfolio of Josua | Homepage",
   description: "Homepage of Josua Ronaldo",
+  openGraph: {
+    title: "Portfolio of Josua Ronaldo",
+    description: "Explore my projects, skills, and experience in web development & design.",
+    url: "https://www.josuaronaldo.my.id",
+    siteName: "Josua Ronaldo Portfolio",
+    images: [
+      {
+        url: "https://www.josuaronaldo.my.id/cover.png", 
+        width: 1200,
+        height: 630,
+        alt: "Josua Ronaldo Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function HomePage() {
